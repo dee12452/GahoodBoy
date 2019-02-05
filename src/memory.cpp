9f68eebc66387f4c2aac4293da0bc577d9b0c4a3
dee12452 <dee12452@gmail.com>
2019-02-05 08:47:59 -0800

    Edited some nitpicky stuff in memory.cpp

--- src/memory.cpp
+++ src/memory.cpp
@@ -4,7 +4,7 @@ Memory::Memory(const Cartridge &cartridge)
 {
     memorySize = 0xFFFF;
     memoryBytes = (byte *) malloc(sizeof(byte) * (static_cast<unsigned long> (memorySize) + 0x01));
-    for(address i = 0x00; i < memorySize; i += 0x01)
+    for(address i = 0x0000; i < memorySize; i += 0x0001)
     {
         if(i <= 0x8000)
         {
@@ -21,7 +21,7 @@ Memory::Memory(const Memory &other)
 {
     memorySize = 0xFFFF;
     memoryBytes = (byte *) malloc(sizeof(byte) * (static_cast<unsigned long> (memorySize) + 0x01));
-    for(address i = 0; i <= memorySize; i+=0x01)
+    for(address i = 0x0000; i <= memorySize; i += 0x0001)
     {
         memoryBytes[i] = other.memoryBytes[i];
     }
@@ -35,7 +35,7 @@ Memory& Memory::operator=(const Memory &other)
     }
     memorySize = 0xFFFF;
     memoryBytes = (byte *) malloc(sizeof(byte) * (static_cast<unsigned long> (memorySize) + 0x01));
-    for(address i = 0; i <= memorySize; i+=0x01)
+    for(address i = 0x0000; i <= memorySize; i += 0x0001)
     {
         memoryBytes[i] = other.memoryBytes[i];
     }
@@ -48,7 +48,7 @@ Memory::~Memory()
     {
         free(memoryBytes);
     }
-    memorySize = 0x00;
+    memorySize = 0x0000;
 }
 
 byte Memory::read(const address addr) const
@@ -58,6 +58,10 @@ byte Memory::read(const address addr) const
 
 void Memory::write(const address addr, const byte byteToWrite)
 {
+    if(addr >= memorySize)
+    {
+        Gahood::criticalError("Attempted to write to memory at out of bounds address %x", addr & 0xFFFF);
+    }
 	switch (addr)
 	{
 	case 0xFF46: // LCD OAM DMA Transfers
@@ -78,6 +82,6 @@ void Memory::write(const address addr, const byte byteToWrite)
 
 void Memory::dumpToFile(const char *filePath) const
 {
-    Gahood::log("Dumping memory to %s", filePath);
+    Gahood::log("Dumping last memory state to %s", filePath);
     Gahood::writeToFile(filePath, memoryBytes);
 }
