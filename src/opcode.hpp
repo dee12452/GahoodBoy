0651d5ebfdf282f7d06ee9430e0f7cbeddfabcba
dee12452 <dee12452@gmail.com>
2019-02-06 17:00:48 -0800

    Removed the +1 in JR, I think its causing problems

--- src/opcode.hpp
+++ src/opcode.hpp
@@ -275,7 +275,7 @@ inline cycle JR(Memory &memory, address &programCounter, const bool condition)
 {
 	if (condition)
 	{
-		programCounter += 0x01 + static_cast<signed char> (memory.read(programCounter + 0x01));
+		programCounter += static_cast<signed char> (memory.read(programCounter + 0x01));
 		return 12;
 	}
 	else
