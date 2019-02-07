820aaad58e643acb4f4cfb9956269d2a0d6a9356
dee12452 <dee12452@gmail.com>
2019-02-07 08:11:49 -0800

    Fixed compile warning with bitwise operators

--- src/memory.cpp
+++ src/memory.cpp
@@ -76,7 +76,7 @@ void Memory::write(const address addr, const byte byteToWrite)
 	}
     case 0xFF41: // LCD status register; bit 0-2 are Read Only
     {
-        if(byteToWrite & 0x07 == 0x07)
+        if((byteToWrite & 0x07) == 0x07)
         {
             Gahood::criticalError("Address %x: Tried writing %x to Read Only bits", addr, byteToWrite);
         }
