6a13dd94236efe167d8ab9fc12cb8fe09470f947
dee12452 <dee12452@gmail.com>
2019-02-28 08:11:09 -0800

    Fixed issues with shifting (RR RL)

--- src/opcode_prefix.hpp
+++ src/opcode_prefix.hpp
@@ -3,39 +3,37 @@
 
 #include "opcode.hpp"
 
-inline cycle RL(byte &flags, byte &reg)
-{
-	const bool carry = (reg & 0x80) == 0x80;
-	reg <<= 1;
-	reg += carry ? 0x01 : 0x00;
-	setZeroFlag(flags, reg == 0x00);
-	setSubtractFlag(flags, false);
-	setHalfCarryFlag(flags, false);
-	setCarryFlag(flags, carry);
-	return 8;
-}
-
 inline cycle RLC(byte &flags, byte &reg)
 {
 	const bool carry = (reg & 0x80) == 0x80;
 	reg <<= 1;
-	reg += getCarryFlag(flags) ? 0x01 : 0x00;
+	reg += carry ? 0x01 : 0x00;
+	if(carry)
+	{
+		setCarryFlag(flags, carry);
+	}
+    else
+	{
+		resetFlags(flags);
+	}
 	setZeroFlag(flags, reg == 0x00);
-	setSubtractFlag(flags, false);
-	setHalfCarryFlag(flags, false);
-	setCarryFlag(flags, carry);
 	return 8;
 }
 
-inline cycle RR(byte &flags, byte &reg)
+inline cycle RL(byte &flags, byte &reg)
 {
-	const bool carry = (reg & 0x01) == 0x01;
-	reg >>= 1;
-	reg |= carry ? 0x80 : 0x00;
+	const bool carry = (reg & 0x80) == 0x80;
+	reg <<= 1;
+	reg += getCarryFlag(flags) ? 0x01 : 0x00;
+	if(carry)
+	{
+		setCarryFlag(flags, carry);
+	}
+    else
+	{
+		resetFlags(flags);
+	}
 	setZeroFlag(flags, reg == 0x00);
-	setSubtractFlag(flags, false);
-	setHalfCarryFlag(flags, false);
-	setCarryFlag(flags, carry);
 	return 8;
 }
 
@@ -43,11 +41,33 @@ inline cycle RRC(byte &flags, byte &reg)
 {
 	const bool carry = (reg & 0x01) == 0x01;
 	reg >>= 1;
-	reg |= getCarryFlag(flags) ? 0x80 : 0x00;
+	reg |= carry ? 0x80 : 0x00;
+	if(carry)
+	{
+		setCarryFlag(flags, carry);
+	}
+    else
+	{
+		resetFlags(flags);
+	}
 	setZeroFlag(flags, reg == 0x00);
-	setSubtractFlag(flags, false);
-	setHalfCarryFlag(flags, false);
-	setCarryFlag(flags, carry);
+	return 8;
+}
+
+inline cycle RR(byte &flags, byte &reg)
+{
+	const bool carry = (reg & 0x01) == 0x01;
+	reg >>= 1;
+	reg |= getCarryFlag(flags) ? 0x80 : 0x00;
+	if(carry)
+	{
+		setCarryFlag(flags, carry);
+	}
+    else
+	{
+		resetFlags(flags);
+	}
+	setZeroFlag(flags, reg == 0x00);
 	return 8;
 }
 
@@ -55,25 +75,29 @@ inline cycle SLA(byte &flags, byte &reg)
 {
 	setCarryFlag(flags, (reg & 0x80) == 0x80);
 	reg <<= 1;
-	setSubtractFlag(flags, false);
-	setHalfCarryFlag(flags, false);
+	if(!getCarryFlag(flags))
+	{
+		resetFlags(flags);
+	}
 	setZeroFlag(flags, reg == 0x00);
 	return 8;
 }
 
 inline cycle SRA(byte &flags, byte &reg)
 {
+	setCarryFlag(flags, (reg & 0x01) == 0x01);
 	reg >>= 1;
-	setCarryFlag(flags, false);
-	setSubtractFlag(flags, false);
-	setHalfCarryFlag(flags, false);
+	if(!getCarryFlag(flags))
+	{
+		resetFlags(flags);
+	}
 	setZeroFlag(flags, reg == 0x00);
 	return 8;
 }
 
 inline cycle SWAP(byte &flags, byte &reg)
 {
-	const byte lowerNibleToUpper = (0x0F & reg) << 4; // Get lower middle and move to upper
+	const byte lowerNibleToUpper = (reg & 0x0F) << 4; // Get lower middle and move to upper
 	reg >>= 4; // Move upper nible to lower
 	reg |= lowerNibleToUpper; // Put the lower nibles back into the upper nible of the register
 	setZeroFlag(flags, reg == 0);
@@ -87,9 +111,11 @@ inline cycle SRL(byte &flags, byte &reg)
 {
 	setCarryFlag(flags, (reg & 0x01) == 0x01);
 	reg >>= 1;
+	if(!getCarryFlag(flags))
+	{
+		resetFlags(flags);
+	}
 	setZeroFlag(flags, reg == 0x00);
-	setSubtractFlag(flags, false);
-	setHalfCarryFlag(flags, false);
 	return 8;
 }
 
